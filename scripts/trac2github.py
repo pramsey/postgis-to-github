47c084ae2cc09d187ea69d7d2d0bffa738d20380
Paul Ramsey <pramsey@cleverelephant.ca>
2017-10-16 05:23:21 -0700

    draft

--- scripts/trac2github.py
+++ scripts/trac2github.py
@@ -13,6 +13,8 @@ import datetime
 import psycopg2
 from psycopg2.extras import RealDictConnection, RealDictCursor, Json
 from psycopg2.extensions import register_type
+import requests
+import pprint
 
 ###############################################################################
 # CONFIGURATION
@@ -32,7 +34,7 @@ usermap = {
     "pracine":"pierre-racine", 
     "chodgson":"chrishodgson", 
     "colivier":"ocourtin", 
-    "dblasby":"DBlasby", 
+    "dblasby":"dblasby", 
     "devrim":"devrimgunduz", 
     "dustymugs":"dustymugs", 
     "dzwarg":"dzwarg", 
@@ -130,11 +132,14 @@ def main():
     revmap = load_revmap(revmapfile)
     labelmap = load_labelmap(repo)
     milestonemap = load_milestonemap(repo)
-    validate_usermap(hub)
+    # validate_usermap(hub)
     
+    for issue in get_issues(conn, repo, first_id=1501, limit=1):
+        logger.info("got issue...")
+        
     # For each ticket
     # 
-    # for ticket in get_trac_tickets(conn, first_ticket=1800, max_tickets=10):
+    # for ticket in get_issues(conn, first_ticket=1800, max_tickets=10):
     #     print "======== %s" % ticket.get("id")
     #     print ticket.get("summary")
     #     print "----"
@@ -146,27 +151,146 @@ def main():
 
 ###############################################################################
 
+def get_issues(conn, repo, first_id=1, limit=10):
+    
+    for ticket in get_trac_tickets(conn, first_ticket=first_id, max_tickets=limit):
+        issue = {}
+        issue["title"] = ticket["summary"]
+        issue["body"] = format_body(ticket)
+        issue["created_at"] = ticket["createtime"].isoformat()
+        issue["updated_at"] = ticket["changetime"].isoformat()
+        issue["assignee"] = trac_user_get_github_user(ticket["owner"])
+        issue["closed"] = (ticket["status"] == 'closed')
+        
+        # Take labels from trac ticket state
+        labels = []
+        for k in ["type", "component", "priority", "resolution"]:
+            label = trac_label_get_github_label(k, ticket[k], repo)
+            if label:
+                labels.append(label.name)
+        if labels:
+            issue["labels"] = labels
+        
+        # Ensure milestone exists in github (already, or create it)
+        # before applying to the issue
+        milestone = trac_milestone_get_github_milestone(ticket["milestone"], conn, repo)
+        if milestone:
+            issue["milestone"] = milestone.title
+        
+        # Get comments and attachments in order and add to 
+        # the issue
+        issue["comments"] = []
+        for state in get_trac_comments_and_attachments(conn, ticket["id"]):
+            comment = {}
+            comment["created_at"] = state["createtime"].isoformat()
+            # Comment
+            if state["type"] == 'comment':
+                comment["body"] = format_comment(state)
+            # Attachment
+            else:
+                comment["body"] = format_attachment(state)
+            issue["comments"].append(comment)
+
+        # Log result
+        logger.info("Formatted Ticket/Issue #%s" % ticket["id"])
+        logger.debug(pprint.pformat(issue))
+        yield issue
+                
+
+
+def format_body(ticket):
+    md = "**Reported by: %s**\n\n" % trac_user_get_github_user(ticket["reporter"], fallback_to_trac=True)
+    md += md_from_trac(ticket["description"])
+    return md
+        
+def format_comment(state):
+    md = "**Author: %s**\n\n" % trac_user_get_github_user(state["author"], fallback_to_trac=True)
+    md += md_from_trac(state["description"])
+    return md
+
+def format_attachment(state):    
+    md = "**Attachment: %s**\n\n" % (attachment_url_prefix + state["filename"])
+    md += md_from_trac(state["description"])
+    return md
+
+###############################################################################
+# Use the Github beta Issue import API
+# https://gist.github.com/jonmagic/5282384165e0f86ef105
+# {
+#   "issue": {
+#     "title": "Imported from some other system",
+#     "body": "...",
+#     "created_at": "2014-01-01T12:34:58Z",
+#     "closed_at": "2014-01-02T12:24:56Z",
+#     "updated_at": "2014-01-03T11:34:53Z",
+#     "assignee": "jonmagic",
+#     "milestone": 1,
+#     "closed": true,
+#     "labels": [
+#       "bug",
+#       "low"
+#     ]
+#   },
+#   "comments": [
+#     {
+#       "created_at": "2014-01-02T12:34:56Z",
+#       "body": "talk talk"
+#     }
+#   ]
+# }
+#
+# curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" \
+#   -H "Accept: application/vnd.github.golden-comet-preview+json" \
+#   -d '{"issue":...}' \
+#   https://api.github.com/${GITHUB_REPO}/${GITHUB_USER}/foo/import/issues
+
+def github_create_issue(issue_dict):
+    
+    url = "https://api.github.com/repos/%s/%s/import/issues" % (github_user, github_repo)
+    logger.debug("POST to %s" % url)
+    headers = {
+        "Authorization": github_password, 
+        "Content-Type": "application/json",
+        "Accept": "vnd.github.golden-comet-preview+json"
+        }
+    r = requests.post("http://httpbin.org/post", data=issue_dict)
+    if args.debug:
+        print(r.text)
+    
+###############################################################################
+
+def trac_user_get_github_user(trac_user, fallback_to_trac=False):
+    if usermap.get(trac_user):
+        return usermap.get(trac_user)
+    elif fallback_to_trac:
+        return trac_user
+    else:
+        return github_user
+
+###############################################################################
+
 def trac_label_get_github_label(trac_key, trac_value, repo):
     
     # Do we have a configuration for this particular
     # trac key/value combination?
     if not traclabelmap.get(trac_key):
         return None
-    else if not traclabelmap[trac_key].get(trac_value):
+    elif not traclabelmap[trac_key].get(trac_value):
         return None
     
     labelcolor = traclabelmap[trac_key][trac_value].split(';')
     label = labelcolor[0]
-    if labelcolor[1]:
+    if len(labelcolor) > 1:
         color = labelcolor[1]
     else:
         color = github_default_label_color
     
     # Yes, we already have this label set up, 
     # so just return the github label object.
-    if labelmap.get(label):
+    if labelmap.get(label) or labelmap.get(label.lower()):
         return labelmap.get(label)
-        
+    
+    logger.info("Creating new github label: %s" % label)
     # We have a configuration but it doesn't 
     # exist in github yet, so create it there.
     return repo.create_label(label, color)
@@ -180,6 +304,7 @@ def trac_milestone_get_github_milestone(trac_milestone, conn, repo):
     if not ms:
         raise Exception("milestone '%s' not found in trac database" % trac_milestone)
     
+    logger.debug(pprint.pformat(ms))
     if ms["due"]:
         return repo.create_milestone(title=ms["name"], state=ms["state"], due_on=ms["due"])
     else:
@@ -244,9 +369,25 @@ def md_from_trac_italic(m):
 
 def md_from_trac_revision(m):
     if m:
+        h = revmap.get(m.group(2))
+        if h:
+            return m.group(1)+h+m.group(3)
+    
+    return None
+
+def md_from_trac_revision_first(m):
+    if m:
         h = revmap.get(m.group(1))
         if h:
-            return h + m.group(2)
+            return h+m.group(2)
+    
+    return None
+
+def md_from_trac_revision_last(m):
+    if m:
+        h = revmap.get(m.group(2))
+        if h:
+            return m.group(1)+h
     
     return None
 
@@ -272,7 +413,11 @@ def md_from_trac(s):
     s = re.sub("\s$", "", s)
     
     # Revision numbers
-    p = re.compile('r(\d+)(\W)')
+    p = re.compile('(\s)r(\d+)$')
+    s = p.sub(md_from_trac_revision_last, s)
+    p = re.compile('^r(\d+)(\W)')
+    s = p.sub(md_from_trac_revision_first, s)
+    p = re.compile('(\s)r(\d+)(\W)')
     s = p.sub(md_from_trac_revision, s)
     
     return s
@@ -292,8 +437,9 @@ def get_trac_tickets(conn, first_ticket=None, max_tickets=None):
     sql = """SELECT id, type, owner, reporter, milestone, 
                 status, resolution,
                 summary, description, 
-                to_timestamp(time / 1000000) as posixtime, 
-                to_timestamp(changetime / 1000000) as modifiedtime
+                component, priority,
+                to_timestamp(time / 1000000) as createtime, 
+                to_timestamp(changetime / 1000000) as changetime
             FROM ticket
             WHERE id >= %s
             ORDER BY id ASC
@@ -312,7 +458,7 @@ def get_trac_tickets(conn, first_ticket=None, max_tickets=None):
 
 def get_trac_comments(conn, ticket):
     sql = """SELECT ticket, 
-                case when time > 0 then to_timestamp(time / 1000000) else NULL end as posixtime, 
+                case when time > 0 then to_timestamp(time / 1000000) else NULL end as createtime, 
                 author, newvalue AS comment
             FROM ticket_change
             WHERE field = 'comment'
@@ -337,9 +483,52 @@ def get_trac_milestone(conn, milestone):
         return cur.fetchone()
 
 ###############################################################################
+#
+# {
+#  "ticket": 1234, 
+#  "type": "comment", # comment|attachment
+#  "createtime": "2017-01-01 01:01:01",
+#  "author": "pramsey",
+#  "description": "some stuff",
+#  "filename": ""
+# }
+#
+def get_trac_comments_and_attachments(conn, ticket):
+    sql = """SELECT 
+                ticket, 
+                'comment' AS type,
+                case when time > 0 then to_timestamp(time / 1000000) else NULL end as createtime, 
+                author, 
+                newvalue AS description,
+                NULL AS filename
+            FROM ticket_change
+            WHERE field = 'comment'
+                AND newvalue <> ''
+                AND ticket = %s
+            UNION
+            SELECT 
+                id::integer AS ticket,
+                'attachment' AS type,
+                case when time > 0 then to_timestamp(time / 1000000) else NULL end as createtime, 
+                author,
+                description,
+                filename
+            FROM attachment
+            WHERE id = %s::text
+            AND type = 'ticket'
+            ORDER BY ticket, createtime
+            """
+    with conn.cursor() as cur:
+        cur.execute(sql, (ticket,ticket))
+        for r in cur:
+            yield r
+
+###############################################################################        
 
 def get_trac_attachments(conn, ticket):
-    sql = """SELECT id, filename, time / 1000000 as posixtime, author 
+    sql = """SELECT id, filename, 
+            case when time > 0 then to_timestamp(time / 1000000) else NULL end as createtime, 
+            author 
             FROM attachment
             WHERE id = %s
             ORDER BY id ASC, time ASC
